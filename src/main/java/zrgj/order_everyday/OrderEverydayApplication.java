aea939dd113a4985f1b6f98de4b97fce313703b8
MuZi-lh <61682147+MuZi-lh@users.noreply.github.com>
2022-02-24 13:57:02 +0800

    init git repository

---
+++ src/main/java/zrgj/order_everyday/OrderEverydayApplication.java
@@ -0,0 +1,13 @@
+package zrgj.order_everyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEverydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderEverydayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zrgj/order_everyday/OrderEverydayApplicationTests.java
@@ -0,0 +1,13 @@
+package zrgj.order_everyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEverydayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
