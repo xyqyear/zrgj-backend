54fc673c0a4f9886b0d5362bc10f475c903b4447
xyqyear <215908558@qq.com>
2022-02-27 09:58:34 +0800

    remove unnecessary imports

--- src/main/java/zrgj/order_everyday/controller/OrderController.java
+++ src/main/java/zrgj/order_everyday/controller/OrderController.java
@@ -7,9 +7,7 @@ import com.auth0.jwt.interfaces.Claim;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
